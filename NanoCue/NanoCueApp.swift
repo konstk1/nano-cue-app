050d775e6d3178f573bd989e14f0bb1b72469f42
Konstantin Klitenik <5853429+konstk1@users.noreply.github.com>
2025-09-04 20:34:41 -0400

    Create Xcode project

---
+++ NanoCue/NanoCueApp.swift
@@ -0,0 +1,17 @@
+//
+//  NanoCueApp.swift
+//  NanoCue
+//
+//  Created by Konstantin Klitenik on 9/4/25.
+//
+
+import SwiftUI
+
+@main
+struct NanoCueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NanoCueTests/NanoCueTests.swift
@@ -0,0 +1,16 @@
+//
+//  NanoCueTests.swift
+//  NanoCueTests
+//
+//  Created by Konstantin Klitenik on 9/4/25.
+//
+
+import Testing
+
+struct NanoCueTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
